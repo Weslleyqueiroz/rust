39b47a1bbbbce5f31270cdd507c72c91b981c05e
Weslleyqueiroz <weslleyqueiroz773@gmail.com>
2025-02-26 19:16:05 -0300

    Primeiro commit em rust

---
+++ inversaoString.rs
@@ -0,0 +1,19 @@
+use std::io;
+
+fn main(){
+    let mut _escreva = String::new();
+    println!("Digite uma palavra : ");
+
+    io::stdin()
+    .read_line(&mut _escreva)
+    .expect("Erro, digite novamente!");
+
+    println!("Você digitou {_escreva}");
+
+    reverse(&_escreva);
+}
+
+    fn reverse(input: &str){
+        let reversed: String = input.chars() .rev() .collect();
+        println!("A palavra invertida é : {} ",reversed);
+    }
